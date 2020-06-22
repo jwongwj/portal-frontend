9a95ed9646b946cd1d6ffd5972b900d0d61d97a2
Jayden <wwj.jayden@gmail.com>
2020-06-22 21:50:15 +0800

    Basic authentication with firebase

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,25 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
-  extends: ["plugin:vue/essential", "@vue/airbnb"],
+  extends: ['plugin:vue/essential', '@vue/airbnb'],
   parserOptions: {
-    parser: "babel-eslint"
+    parser: 'babel-eslint',
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "vue/no-parsing-error": "off",
-    "import/no-unresolved": "off",
-    "space-before-function-paren": "off",
-    "no-alert": "off",
-    "no-console": "off",
-    "no-debugger": "off",
-    "no-plusplus": "off",
-    "max-len": "off",
-    "no-param-reassign": "off",
-    "no-return-assign": "off",
-    "consistent-return": "off",
-    "operator-linebreak": "off",
-    "linebreak-style": "off",
-    "vue/no-parsing-error": "off"
-  }
+    'vue/no-parsing-error': 'off',
+    'import/no-unresolved': 'off',
+    'space-before-function-paren': 'off',
+    'no-alert': 'off',
+    'no-console': 'off',
+    'no-debugger': 'off',
+    'no-plusplus': 'off',
+    'max-len': 'off',
+    'no-param-reassign': 'off',
+    'no-return-assign': 'off',
+    'consistent-return': 'off',
+    'operator-linebreak': 'off',
+    'linebreak-style': 'off',
+  },
 };
